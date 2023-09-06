cdc6a99a5d4d603622f9589903e875dace377622
Sandeep Kumar Singh <sandeep908singh908@gmail.com>
2023-09-06 17:33:28 +0530

    Create Histogram.java

---
+++ Histogram.java
@@ -0,0 +1,25 @@
+import java.util.* ;
+class Histogram {
+  public static void main(String[] args) {
+    // System.out.println("Hello world!");
+
+    int[] arr = {6 ,2 ,5 ,5 ,5 ,1 ,6} ;
+    int maxArea = 0 ;
+    int n = arr.length ;
+    for(int i=0 ;i<n ;i++){
+      int count=1 ,num = arr[i] ;
+      int j=i-1;
+      while(j>0 && num<=arr[j]){
+        count++ ;
+        j-- ;
+      }
+      j=i+1;
+      while(j<n-1 && num<=arr[j]){
+        count++ ;
+        j++;
+      }
+      maxArea = Math.max(maxArea ,count*arr[i]) ;
+    }
+    System.out.println(maxArea);
+  }
+}
